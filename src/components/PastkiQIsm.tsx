785457a2079418fca054131bea3524e26b708ed1
zarifa5655 <zarifagafurova55@gmail.com>
2025-03-26 19:34:28 +0500

    Headerni-paski-qismi

---
+++ src/components/PastkiQIsm.tsx
@@ -0,0 +1,50 @@
+import rasm from"../../src/assets/iconka/Package.png"
+import rasm2 from"../../src/assets/iconka/Trophy.png"
+import rasm3 from"../../src/assets/iconka/CreditCard.png"
+import rasm4 from"../../src/assets/iconka/Headphones.png"
+function PastkiQIsm(){
+  return (
+      <div className=" flex items-center justify-between  border-1 border-gray-300 p-4 gap-[28p] px-[300px] ">
+          
+          <div className="flex p-4  pr-[50px]   border-r-1 border-gray-300 ml-[20px]  items-center gap-[16px] justify-between">
+              <div><img src={rasm} alt="" /></div>
+              <div className="flex flex-col   gap-1">
+                  <h1 className="text-[14px] uppercase decoration-1 text-[#191C1F]">Fasted Delivery</h1>
+              <h1 className="text-[#5F6C72]  text-[14px]">Delivery in 24/H</h1>
+              </div>
+              
+          </div>
+
+                    <div className="flex p-4 gap-[16px] border-r-1 items-center  pr-[50px] border-gray-300 justify-between">
+              <div><img src={rasm2} alt="" /></div>
+              <div className="flex flex-col   gap-1">
+                  <h1 className="text-[14px] uppercase decoration-1 text-[#191C1F]">24 Hours Return</h1>
+              <h1 className="text-[#5F6C72]  text-[14px]">100% money-back guarantee</h1>
+              </div>
+              
+          </div>
+
+                    <div className="flex p-4 border-gray-300 pr-[50px] gap-[16px] border-r-1 items-center justify-between">
+              <div><img src={rasm3} alt="" /></div>
+              <div className="flex flex-col   gap-1">
+                  <h1 className="text-[14px] uppercase decoration-1 text-[#191C1F]">Secure Payment</h1>
+              <h1 className="text-[#5F6C72]  text-[14px]">Your money is safe</h1>
+              </div>
+              
+          </div>
+
+                    <div className="flex p-4  gap-[16px] items-center justify-between">
+              <div><img src={rasm4} alt="" /></div>
+              <div className="flex flex-col   gap-1">
+                  <h1 className="text-[14px] uppercase decoration-1 text-[#191C1F]">Support 24/7</h1>
+              <h1 className="text-[#5F6C72]  text-[14px]">Live contact/message</h1>
+              </div>
+              
+          </div>
+          
+
+    </div>
+  )
+}
+
+export default PastkiQIsm
